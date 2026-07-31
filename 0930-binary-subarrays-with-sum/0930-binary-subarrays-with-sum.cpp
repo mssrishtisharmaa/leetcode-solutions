d94bfb1c3755d0b634dc7225a821bac36322d78e
Srishti <ms.srishtisharmaa@gmail.com>
2026-07-31 23:44:59 +0530

    Time: 0 ms (100%), Space: 32.7 MB (63.2%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,19 +1,51 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return atmost(nums, goal) - atmost(nums, goal - 1);
         
-       map<int , int> mpp;
-       mpp[0] = 1;
-       int prefix_sum = 0;
-       int cnt = 0;
-       for(int i = 0; i < nums.size() ; i++){
-        prefix_sum += nums[i];
-        int remove = prefix_sum - goal;
-        cnt += mpp[remove];
-        mpp[prefix_sum] += 1;
-       }
-       return cnt;
     }
+private: 
+  int atmost(vector<int>& nums, int goal){
+    if (goal < 0) return 0;
+    int l = 0;
+    int r = 0;
+    int sum = 0;
+    int cnt = 0;
+
+    while ( r < nums.size()){
+        sum += nums[r];
+        
+
+        while(sum> goal){
+            sum -= nums[l];
+            l++;
+        }
+        cnt += (r - l +1);
+        r++;
+    }
+    return cnt;
+
+  }
 };
 
+
+
+// // same sol as PROBLEM : 560 [ it was used in array for + , - and  zero]
+
+// same solution will work but ( not most optimal one )
+
+// there fore we need to optimize it 
+
+// sol :
+// map<int , int> mpp;
+//        mpp[0] = 1;
+//        int prefix_sum = 0;
+//        int cnt = 0;
+//        for(int i = 0; i < nums.size() ; i++){
+//         prefix_sum += nums[i];
+//         int remove = prefix_sum - goal;
+//         cnt += mpp[remove];
+//         mpp[prefix_sum] += 1;
+//        }
+//        return cnt;
 
